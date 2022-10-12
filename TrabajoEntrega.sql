997e462d3cbfda754daac80539038df5cdb623f3
juaaanez <juan-esteban-06@hotmail.com>
2022-10-12 19:35:12 -0300

    proyecto

--- TrabajoEntrega.sql
+++ TrabajoEntrega.sql
@@ -49,3 +49,35 @@ tipo varchar (255),
 FOREIGN KEY (id)
 REFERENCES Productos (tipo_delivery)
 );
+
+-- INSERTS --
+
+insert into Consumidores (Id, Nombre, Apellido, Direccion) values (1, 'franco', 'gonzalez', 'derqui');
+insert into Consumidores (Id, Nombre, Apellido, Direccion) values (2, 'juan', 'perez', 'francia');
+insert into Consumidores (Id, Nombre, Apellido, Direccion) values (3, 'maria', 'serrano', 'florida');
+
+insert into Aplicacion (Id, Pedido_descripcion, Forma_Pago, pedidoID) values (1, 'comida', 3, 3);
+insert into Aplicacion (Id, Pedido_descripcion, Forma_Pago, pedidoID) values (2, 'helado', 1, 1);
+insert into Aplicacion (Id, Pedido_descripcion, Forma_Pago, pedidoID) values (3, 'perfmueria', 4, 2);
+
+insert into Forma_De_Pago (id, tipo_pago) values (1, 'debito');
+insert into Forma_De_Pago (id, tipo_pago) values (3, 'efectivo');
+insert into Forma_De_Pago (id, tipo_pago) values (4, 'transferencia');
+
+insert into Restaurante (id, NombreR, Producto) values (1, 'antares', '2');
+insert into Restaurante (id, NombreR, Producto) values (2, 'gluck', '3');
+insert into Restaurante (id, NombreR, Producto) values (3, 'farmacia', '4');
+
+insert into Productos (id, Tipo, Precio, Tipo_delivery) values (1, 'bebidas', 100, 1);
+insert into Productos (id, Tipo, Precio, Tipo_delivery) values (2, 'comida', 150, 2);
+insert into Productos (id, Tipo, Precio, Tipo_delivery) values (3, 'helado', 200, 3);
+insert into Productos (id, Tipo, Precio, Tipo_delivery) values (4, 'perfmueria', 120, 4);
+
+insert into Delivery (id, tipo) values (1, 'moto');
+insert into Delivery (id, tipo) values (2, 'auto');
+insert into Delivery (id, tipo) values (3, 'bicicleta');
+insert into Delivery (id, tipo) values (4, 'retiro');
+
+
+
+
